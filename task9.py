3a1392aded099e2e1011c513a71a19192f3f56ac
ekaterina <ms.katiush@list.ru>
2022-10-22 18:42:03 +0300

    task9

---
+++ task9.py
@@ -0,0 +1,9 @@
+z = int(input ('Введите номер четверти:'))
+if z==1:
+    print ('x>0, y>0')
+elif z==2:
+    print ('x<0, y>0')
+elif z==3:
+    print ('x<0, y<0')
+else:
+    print ('x>0, y<0')        

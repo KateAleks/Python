41d18ae9c4be66345c56b08deb80e9f6db92ebb6
Ekaterina <111282568+KateAleks@users.noreply.github.com>
2022-11-21 20:54:21 +0300

    Delete task15.py

--- task15.py
+++
@@ -1,8 +0,0 @@
-#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-number = int(input ('Введите число  '))
-my_list = []
-multipl = 1
-for i in range(1,number +1):
-    multipl*=i
-    my_list.append(multipl) 
-    print(f'Результат умножения всех элементов списка = {my_list}')
